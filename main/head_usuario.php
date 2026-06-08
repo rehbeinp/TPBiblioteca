822367106b3474e7ddf44bdc60671d94fbd65f50
Paula Rehbein <paula.rehbein@acad.ufsm.br>
2026-06-07 22:40:15 -0300

    Adicionei mais botoes no menu

--- main/head_usuario.php
+++ main/head_usuario.php
@@ -6,10 +6,11 @@
 <body>
     <form  method='POST'>
 		<button type='submit' name='btn_logout'>Sair</button> 
-    <p> <button type='submit' name='btn_listar_livros'>Ver todos os livros</button>
+    <p> <button type='submit' name='btn_home'>Menu Principal</button> 
+        <button type='submit' name='btn_listar_livros'>Acervo de livros</button>
         <button type='submit' name='btn_listar_historico'>Histórico de retiradas</button>
-        <button type='submit' name='btn_home'>Voltar ao Menu Principal</button> 
-
+        <button type='submit' name='btn_multas'>Minhas multas</button>
+        <button type='submit' name='btn_conta_user'>Minha conta</button>
     </p>
 	</form>
 </body>
@@ -38,4 +39,13 @@ if(isset($_POST["btn_home"])){
     header("location: menu_usuario.php");
 }
 
+if(isset($_POST["btn_multas"])){
+    unset($_SESSION['status']);
+    header("location: menu_multas.php");
+}
+
+if(isset($_POST["btn_conta_user"])){
+    unset($_SESSION['status']);
+    header("location: conta_usuario.php");
+}
 ?>

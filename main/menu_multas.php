d38cdcb5ee4e7242cbe1a6189181cb2988369c23
Paula Rehbein <paula.rehbein@acad.ufsm.br>
2026-06-07 22:43:16 -0300

    ajustando detalhes

--- main/menu_multas.php
+++ main/menu_multas.php
@@ -1,4 +1,3 @@
-
 <?php
 include "head_usuario.php";
 
@@ -9,7 +8,6 @@ if(isset($_SESSION['status']) and $_SESSION['status']!= "" ){
     $user = $_SESSION['status'];
 
     $con = mysqli_connect("localhost", "root", "123456", "biblioteca", "3306");
-
     if (mysqli_connect_errno()) {
         echo "Falhou devido a conexao com Mysql:" . mysqli_connect_error();
     }
@@ -23,13 +21,13 @@ if(isset($_SESSION['status']) and $_SESSION['status']!= "" ){
                 e.data_devolucao_prevista,
                 m.valor as valor_multa,
                 m.motivo as motivo_multa,
-                m.paga as multa_paga
+                cast(m.paga as SIGNED) as multa_paga
             from emprestimos e
             inner join usuarios u on e.usuario_id = u.id
             inner join livros l on e.livro_id = l.id
-            left join multas m on m.emprestimo_id = e.id
+            inner join multas m on m.emprestimo_id = e.id
             where u.email='$user'
-            order by e.data_retirada desc, e.data_devolucao_prevista desc";
+            order by m.paga asc, e.data_retirada desc, e.data_devolucao_prevista desc";
 
     $exesql_retirados = mysqli_query($con, $sql_retirados);
     mysqli_close($con);
@@ -47,12 +45,12 @@ else{
 </head>
 <body>
     <br>
-    <h2>Bem-Vindo ao Histórico de Retiradas</h2> 
+    <h2>Histórico de Multas Registradas</h2> 
     <form  method='POST'>
         <div>
            <?php 
            if ($num_linhas >= 1) {
-            echo "<h3>Livros que você já retirou:</h3>";
+
             while ($resul = mysqli_fetch_assoc($exesql_retirados)) {
                 echo "___________________________________________________________ <br> <br>";
                 echo "<strong>Título: </strong>" . $resul['titulo']. " <br>";
@@ -63,17 +61,12 @@ else{
                 if( $resul['data_devolucao'] != null){
                     echo "<strong>Data da Devolução: </strong>" . $resul['data_devolucao']. " <br>";
                 }
-                if( $resul['valor_multa'] == null){
-                    echo "<strong>Multa: </strong> Sem multa. :) <br><br>";
-                }
-                else{
-                    echo" - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - <br>";
-                    if($resul['multa_paga'] == 1) {echo "<strong> Multa quitada :) </strong> <br>";}
-                    else{ echo "<strong> Multa em haver :( </strong> <br> Venha pagar para não aumentar ;) <br>";}
-                    echo "<strong>Valor multa: </strong> R$" . $resul['valor_multa']. " <br>";
-                    echo "<strong>Motivo da Multa: </strong>" . $resul['motivo_multa']. "  <br><br> ";
-                    
-                }
+                echo" - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - <br>";
+                if($resul['multa_paga'] == 1) {echo "<strong> Multa quitada :) </strong> <br>";}
+                else{ echo "<strong> Multa em haver :( </strong> <br> Venha pagar para não aumentar ;) <br> ";}
+                echo "<strong>Valor multa: </strong> R$" . $resul['valor_multa']. " <br>";
+                echo "<strong>Motivo da Multa: </strong>" . $resul['motivo_multa']. " <br><br> ";
+                
             }
         
 
